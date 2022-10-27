c4223ae2e7ab4607ade602fe4ba8bf7caf544e9a
NIKHIL MATTA <67046318+nikhil478@users.noreply.github.com>
2022-10-28 01:16:52 +0530

    Time: 968 ms (79.01%), Space: 19 MB (63.17%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,6 +1,6 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        dg = [0] * (n + 1)
+        dg = [0] * (1000 + 1)
         for a, b in trust:
             dg[a] -= 1 # out
             dg[b] += 1 # in
